5e7c3ddc34c440d30fde2d5085510cfe931ff8d3
Romanist <smilemoon@yandex.ru>
2019-02-05 17:42:34 +0300

    answers to query

--- modules/filmShema.js
+++ modules/filmShema.js
@@ -8,12 +8,12 @@ let filmShema = new Schema({
 	Id: {type: String, required: true},
 	Category: {type: Number, required: true},
 	Name: {type: String, required: true},
-	Production year: {type: Number, required: true},
+	"Production year": {type: Number, required: true},
 	Genres: {type: String, required: true},
 	Genres2: {type: String},
 	Genres3: {type: String},
 	Age: {type: String}
 });
-let Film = mongoose.model('User', filmShema);
+let Film = mongoose.model('Film', filmShema);
 
 module.exports = Film;
--- modules/findFilm.js
+++ modules/findFilm.js
@@ -35,13 +35,57 @@ async function findFilm(ctx) {
 async function generateFilm(data) {
 	let value = data.value
 	console.log(value)
-	let promise = User.find({'id': id}, function (err, user) {}).exec();
-  promise.then(ctx => {
-  	console.log(ctx)
-  	// generateFilm(ctx)
-  }, err => {
-    console.log('err', err)
-  });
+	// let promise = Film.find({'Genres': 'Мультфильм'}, function (err, user) {}).exec();
+ //  promise.then(ctx => {
+ //  	console.log(ctx)
+ //  	// generateFilm(ctx)
+ //  }, err => {
+ //    console.log('err', err)
+ //  });
+
+ 	let category = value.step1 === '1' ? 1 : ((value.step1 === '2') || (value.step1 === '4')) ? '' : 2;
+
+ 	let genre1 = value.step2 === '1' ? 'Комедия' :
+ 		value.step2 === '2' ? 'Триллер' : 
+ 		value.step2 === '3' ? 'Ужасы' : 'Драма';
+ 	let genre2 = value.step2 === '1' ? 'Комедия' :
+ 		value.step2 === '2' ? 'Боевик' : 
+ 		value.step2 === '3' ? 'Фантастика' : 'Мелодрама';
+ 	let genre3 = value.step2 === '1' ? 'Комедия' :
+ 		value.step2 === '2' ? 'Криминал' : 
+ 		value.step2 === '3' ? 'Ужасы' : 'Драма';
+
+ 	let yearStart = value.step3 === '1' ? 0 : 
+ 		value.step3 === '2' ? 1990 : 
+ 		value.step3 === '3' ? 2000 : 2010;
+ 	let yearEnd = value.step3 === '1' ? 1989 : 
+ 		value.step3 === '2' ? 1999 : 
+ 		value.step3 === '3' ? 2009 : 2019;
+
+ 	let age = ((value.step1 === '1') || (value.step1 === '2') || (value.step1 === '3')) ? 100 : 6;
+
+ 	console.log('')
+ 	console.log(category)
+ 	console.log('')
+ 	console.log(genre1, genre2, genre3)
+ 	console.log('')
+ 	console.log(yearStart, yearEnd)
+ 	console.log('')
+ 	console.log(age)
+ 	console.log('')
+
+  Film.count().exec(function (err, count) {
+
+	  // Get a random entry
+	  var random = Math.floor(Math.random() * count)
+
+	  // Again query all users but only fetch one offset by our random #
+	  Film.findOne().skip(random).exec(
+	    function (err, result) {
+	      // Tada! random user
+	      console.log(result) 
+	    })
+	})
 }
 
 module.exports = findFilm;
