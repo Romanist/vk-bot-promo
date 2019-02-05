08d74337be5dd672f6fcdf8c57c575ab16050f7b
Romanist <smilemoon@yandex.ru>
2019-02-05 18:20:44 +0300

    random tracking

--- modules/findFilm.js
+++ modules/findFilm.js
@@ -74,13 +74,15 @@ async function generateFilm(data) {
  	console.log(age)
  	console.log('')
 
-  Film.count().exec(function (err, count) {
+  Film.count({'Production year': { $gte: yearStart, $lte: yearEnd }}).exec(function (err, count) {
 
 	  // Get a random entry
 	  var random = Math.floor(Math.random() * count)
 
 	  // Again query all users but only fetch one offset by our random #
-	  Film.findOne().skip(random).exec(
+	  Film.findOne({
+	  	'Production year': { $gte: yearStart, $lte: yearEnd }
+	  }).skip(random).exec(
 	    function (err, result) {
 	      // Tada! random user
 	      console.log(result) 
